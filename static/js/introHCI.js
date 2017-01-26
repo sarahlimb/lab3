136c85fbd708e5386c9bbbf56c25927a979e0a39
Sarah Limb <slimb.ucsd.edu>
2017-01-26 13:16:06 -0800

    .

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,35 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("The best way is to just observe the noise of the world.");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+/*
+ * Function that prevents the default behavior then change the
+ * color of the project node
+ */
+ function projectClick(e) {
+ 	//prevent the page from reloading
+ 	e.preventDefault();
+
+ 	//in an event handler, $(this) refers to 
+ 	//the object that triggerred the event
+ 	$(this).css("background-color", "#7fff00");
+
+ 	//var containingProject = $(this).closest(".project");
+    //containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } 
+    else {
+       //description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+       $("#.containingProject").fadeOut();
+    }
+ }
 }
